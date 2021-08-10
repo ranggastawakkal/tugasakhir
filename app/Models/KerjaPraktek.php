b2618d0d8b019eb0dd401b8cdfaf1814a08c4382
Rangga Sultan <rangga.awantara@gmail.com>
2021-08-10 22:11:12 +0700

    memperbaiki class model kerja praktek

--- app/Models/KerjaPraktek.php
+++ app/Models/KerjaPraktek.php
@@ -65,12 +65,12 @@ class KerjaPraktek extends Model
     //     return $this->hasOne(PembimbingLapangan::class, 'id', 'id_pemb_lap');
     // }
 
-    public function pembAkd()
+    public function pembimbingAkademik()
     {
         return $this->belongsTo(PembimbingAkademik::class, 'id_pemb_akd');
     }
 
-    public function pembLap()
+    public function pembimbingLapangan()
     {
         return $this->belongsTo(PembimbingLapangan::class, 'id_pemb_lap');
     }
